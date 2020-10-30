7420ffa1130b0280e6664e3c532cb5d9eee2dc9d
basilomp <73391933+basilomp@users.noreply.github.com>
2020-10-30 15:50:47 +0300

    Revert "Project reupload + new homework done"

--- Homework/src/main/java/ru/geekbrains/java/javaone/homework/LessonOne/TaskOne.java
+++
@@ -1,9 +0,0 @@
-//1. Создать пустой проект в IntelliJ IDEA и прописать метод main();
-
-package ru.geekbrains.java.javaone.homework.LessonOne;
-
-public class TaskOne {
-    public static void main(String[] args) {
-        System.out.println("Первое задание.");
-    }
-}
